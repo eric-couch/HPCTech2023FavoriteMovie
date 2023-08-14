4cd1520464407bf2b05404dc6ce8e20992bbb7d4
Eric Couch <ericcouch@yahoo.com>
2023-08-14 15:22:57 -0500

    added skeleton for movie search page.

---
+++ HPCTech2023FavoriteMovie/Client/Pages/Search.razor.cs
@@ -0,0 +1,26 @@
+﻿using HPCTech2023FavoriteMovie.Shared;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace HPCTech2023FavoriteMovie.Client.Pages;
+
+public partial class Search
+{
+    [Inject]
+    public HttpClient Http { get; set; } = new HttpClient();
+    private readonly string OMDBAPIUrl = "https://www.omdbapi.com/?apikey=";
+    private readonly string OMDBAPIKey = "86c39163";
+    public MovieSearchResult movieResult { get; set; }
+
+    public string searchTerm { get; set; }
+
+
+    private async Task SearchOMDB()
+    {
+        movieResult = await Http.GetFromJsonAsync<MovieSearchResult>($"{OMDBAPIUrl}{OMDBAPIKey}&s={searchTerm}");
+        //if (movieResult is not null)
+        //{
+
+        //}
+    }
+}
---
+++ HPCTech2023FavoriteMovie/Shared/MovieSearchResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPCTech2023FavoriteMovie.Shared;
+
+public class MovieSearchResult
+{
+    public MovieSearchResultItem[] Search { get; set; }
+    public int totalResults { get; set; }
+    public string Response { get; set; }
+}
